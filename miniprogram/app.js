59b2720afc28b117dd9513a46ee7c068ec2d6082
unknown <unknown@example.com>
2018-09-19 01:33:56 +0800

    Update app.js

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -18,15 +18,9 @@ App({
       success: res => {
         console.log('[云函数] [login] user openid: ', res.result.openid)
         this.globalData.openid = res.result.openid
-        wx.navigateTo({
-          url: 'pages/userConsole/userConsole',
-        })
       },
       fail: err => {
         console.error('[云函数] [login] 调用失败', err)
-        wx.navigateTo({
-          url: '../deployFunctions/deployFunctions',
-        })
       }
     })
   }
